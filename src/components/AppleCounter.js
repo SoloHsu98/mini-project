9d4e1d9bf1f4603a69ee74475cd59b82a2d0e08f
Wendy <67952236+Wendy-Aike-Maung@users.noreply.github.com>
2022-08-20 14:30:17 +0630

    create useState for counter

--- src/components/AppleCounter.js
+++ src/components/AppleCounter.js
@@ -1,14 +1,34 @@
-import React from 'react'
-import "./AppleCounter.css"; <
-link rel = "stylesheet"
-href = "path/to/font-awesome/css/font-awesome.min.css" > < /link>
+import React, { useState, useEffect } from 'react'
+import "./AppleCounter.css"; 
+import { sqrt } from 'mathjs'
+
+<link rel = "stylesheet" href = "path/to/font-awesome/css/font-awesome.min.css" > < /link>
 
 
 const AppleCounter = () => {
+    const result = "NO";
+    const [counter, setCounter] = useState(0);
+    useEffect(()=> {
+        if(counter % 10 == 0){
+            result = "Yes"
+        }
+    })
+    const changeContent = () => {
+        setCounter( counter - Math.floor(Math.random(counter) * 5 + 1))
+        if(counter<0){
+            setCounter(0);
+        }
+    }
     return ( < div >
-        Total Apples < i class = "fa-brands fa-apple" > < /i> < /
-        div >
+        <h3>Total Apples < i class = "fa-brands fa-apple" > < /i> </h3>
+        <h2 className = "count"> counter </h2>
+        <h4> Exactly in Tens: {result} </h4>
+        <button className = "btn" onClick={()=> setCounter( counter + Math.floor(Math.random(counter) * 5 + 1))}>Add More Apples</button>
+        <button className = "btn" onClick={changeContent}>Eat Some Apples</button>
+
+        
+        < /div >
     )
 }
 
-export default AppleCounter
+export default AppleCounter;
