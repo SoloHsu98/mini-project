0324527546d3a5c5a301238f5be98dd7574c8f49
Wendy <67952236+Wendy-Aike-Maung@users.noreply.github.com>
2022-08-21 11:28:38 +0630

    onClick function also doesn't work

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -1,7 +1,38 @@
-import React from 'react'
+import React, { useState, useEffect }  from 'react'
 import "./ToDoList.css";
+
 const ToDoList = () => {
-    return ( <div > ToDoList < /div>
+    const [text, setText] = useState("")
+    const [todo, setTodo] = useState([])
+    const showList = (e)=>{
+        e.preventDefault();
+        const 
+        
+    }
+    return ( <> 
+        <form className='form'>
+        <div>
+       <label htnlFor="task">Task:</label>
+       <input 
+       type="text"
+        id="task" 
+        name="text" 
+        value={text}
+        onChange={(e)=> setText(e.target.value)} />
+
+       <button className ="btn" type="submit" onClick={showList} disabled>Add</button>
+       </div>
+       </form>
+       {list.map(( , index) =>{
+        return (
+            <ul className='item'>
+                <li>{text} <i class</li>
+            </ul>
+        
+        )
+       }
+       ) }
+        </>
     )
 }
 
