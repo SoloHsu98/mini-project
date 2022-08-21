8a08265654913b1ecd938993b326f13332f8ceee
Wendy <67952236+Wendy-Aike-Maung@users.noreply.github.com>
2022-08-21 11:27:44 +0630

    useEffect if statement doesn't work

--- src/components/AppleCounter.js
+++ src/components/AppleCounter.js
@@ -1,33 +1,45 @@
 import React, { useState, useEffect } from 'react'
 import "./AppleCounter.css"; 
-import 'math'
-
-<link rel = "stylesheet" href = "path/to/font-awesome/css/font-awesome.min.css" > < /link>
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+<link rel = "stylesheet" href = "https://use.fontawesome.com/releases/v5.6.1/css/all.css"></link>
 
 
 const AppleCounter = () => {
-    const result = "NO";
-    const [counter, setCounter] = useState(0);
-    useEffect(()=> {
-        if(counter % 10 == 0){
-            result = "Yes"
-        }
-    })
-    const changeContent = () => {
-        setCounter( counter - Math.floor(Math.random(counter) * 5 + 1))
-        if(counter<0){
-            setCounter(0);
-        }
-    }
-    return ( < div >
-        <h3>Total Apples < i class = "fa-brands fa-apple" > < /i> </h3>
-        <h2 className = "count"> counter </h2>
-        <h4> Exactly in Tens: {result} </h4>
-        <button className = "btn" onClick={()=> setCounter( counter + Math.floor(Math.random(counter) * 5 + 1))}>Add More Apples</button>
-        <button className = "btn" onClick={changeContent}>Eat Some Apples</button>
 
+    const [counter, setCounter] = useState(0);
+    const [result, setResult] = useState('NO');
+    const [color, setColor] = useState('Red')
+
+    useEffect(()=> {
+        let remainder = counter % 10
+        if(remainder !== 0){
+            setResult("YES")
+            setColor('Green')
+        } 
+    },[counter])
+    const changeContent = () => {
+       let response = setCounter( counter - Math.floor(Math.random(counter) * 5 + 1)) 
+       if (response < 0){
+        setCounter(response)
+       }
+       else {
+        setCounter(0)
+       }
+    }
+    //     setCounter(counter - Math.floor(Math.random(counter) * 5 + 1))
+        // if(counter<0){
+            // setCounter(0)
+            
         
-        < /div >
+    
+    return ( <div>
+        <h3>Total Apples <FontAwesomeIcon icon="apple"/> </h3>
+        <h2 className = "count"> {counter} </h2>
+        <p> Exactly in Tens: <span style={{ color }}>{result} </span> </p>
+        <button className = "btn" onClick={()=> setCounter( counter + Math.floor(Math.random(counter) * 5 + 1)) }>Add More Apples</button>
+        <button className = "btn" onClick={changeContent}>Eat Some Apples</button>
+        
+        </div >
     )
 }
 
