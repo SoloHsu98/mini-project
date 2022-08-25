9db934fa60b580d01f6caab39a71861ce67118be
Wendy <67952236+Wendy-Aike-Maung@users.noreply.github.com>
2022-08-25 21:14:52 +0630

    emotion css

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -1,16 +1,9 @@
-<<<<<<< HEAD
-import { css } from "@emotion/react";
-import React, { useState } from "react";
-=======
 import React, { useState } from "react";
 import "./ToDoList.css";
-import { BsX } from "react-icons/bs";
->>>>>>> df1d08963c914cba70940bd8c4017b886f4df526
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTimes } from "@fortawesome/free-solid-svg-icons";
 
 const ToDoList = () => {
-<<<<<<< HEAD
 	const [text, setText] = useState("");
 	const [todo, setTodo] = useState([]);
 
@@ -102,78 +95,3 @@ const styles = {
 		align-items: center;
 	`,
 };
-=======
-  const [text, setText] = useState("");
-  const [todo, setTodo] = useState([]);
-
-  const handleChange = (e) => {
-    setText(e.target.value);
-  };
-
-  const handleKeyDown = (e) => {
-    if (e.key === "Enter") {
-      addToList();
-    }
-  };
-
-  const handleDelete = (idToDelete) => {
-    setTodo(todo.filter((element) => element.id !== idToDelete));
-  };
-
-  const addToList = () => {
-    const id = new Date().getTime().toString();
-    const item = {
-      id: id,
-      subject: text,
-    };
-    setTodo([...todo, item]);
-    setText("");
-  };
-
-  return (
-    <>
-      <div className="inputContainer">
-        <label htmlFor="taskInput" className="inputLabel">
-          Task:
-        </label>
-        <input
-          name="taskInput"
-          className="inputField"
-          value={text}
-          onChange={handleChange}
-          onKeyDown={handleKeyDown}
-        />
-        <button
-          className="addButton"
-          disabled={text.trim() === ""}
-          onClick={addToList}
-        >
-          Add
-        </button>
-      </div>
-      {todo.length > 0 ? (
-        <div className="listContainer">
-          <ul className="todoList">
-            {todo.map((item) => (
-              <li className="listItem" key={item.id}>
-                <div className="listItemContent">
-                  <span>{item.subject}</span>
-                  <FontAwesomeIcon
-                    icon={faTimes}
-                    size={"lg"}
-                    onClick={() => handleDelete(item.id)}
-                  />
-                </div>
-              </li>
-            ))}
-          </ul>
-        </div>
-      ) : (
-        <p>There is nothing to do. {":)"}</p>
-      )}
-    </>
-  );
-};
-
-export default ToDoList;
->>>>>>> df1d08963c914cba70940bd8c4017b886f4df526
